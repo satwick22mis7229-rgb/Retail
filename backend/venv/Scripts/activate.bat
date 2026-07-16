c8506cdab17b64dadd9fc6fe0ca4603aa0330235
Yaswanth <potnuruyaswanthcr7@gmail.com>
2026-07-16 10:23:04 +0530

    refactor: complete Module 2 retail intelligence data model

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\SATWICK\Downloads\smart retail intelliegence platform\backend\venv
+set VIRTUAL_ENV=C:\Learning\WD\Retail\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
